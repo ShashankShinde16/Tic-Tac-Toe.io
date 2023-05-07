6ad7b5b127dbcbb0200c5a51ffb0c7fe3d41634f
ShashankShinde16 <ss9575a@gmail.com>
2023-05-07 19:27:45 +0530

    user Interface

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { useState } from 'react';
 import './App.css';
+
+function Square() {
+  const [val, setVal] = useState(null);
+  function handleEvent(){
+    setVal('X');
+  }
+  return <button className="square" onClick={handleEvent}>{val}</button>;
+}
+
 
 function App() {
   return (
-    <div className='t3'>
-      
+    <div className='t'>
+      <div className="t1">
+        <Square/>
+        <Square/>
+        <Square/>
+      </div>
+      <div className="t2">
+      <Square/>
+        <Square/>
+        <Square/>
+      </div>
+      <div className="t3">
+      <Square/>
+        <Square/>
+        <Square/>
+      </div>
     </div>
   );
 }
