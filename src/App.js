fbdb97ce37617d1e6e20b2796a1a2ed0550f5432
ShashankShinde16 <ss9575a@gmail.com>
2023-05-07 19:47:40 +0530

    App.js
    working of x and o played by player on the alternative turn has been done

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
 import { useState } from 'react';
 import './App.css';
 
-function Square() {
-  const [val, setVal] = useState(null);
-  function handleEvent(){
-    setVal('X');
-  }
-  return <button className="square" onClick={handleEvent}>{val}</button>;
+function Square({value,onSquareClick}) {
+  return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
 
 
 function App() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setsquares] = useState(Array(9).fill(null));
+  function handleClick(i){
+    if (squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setsquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
   return (
     <div className='t'>
       <div className="t1">
-        <Square/>
-        <Square/>
-        <Square/>
+      <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="t2">
-      <Square/>
-        <Square/>
-        <Square/>
+      <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="t3">
-      <Square/>
-        <Square/>
-        <Square/>
+       <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </div>
   );
